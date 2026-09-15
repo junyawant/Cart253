abc508eb00e9183265503618ddac22483ee44dda
junya <jomarkson01@gmail.com>
2026-09-15 18:17:11 -0400

    Changing the title and fixing the file for submission

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -1,43 +1,45 @@
-/**
- * Doomsday
- * Erica Galvez & Sabrina Rath
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
- createCanvas(640, 640); 
+function setup (){
+    createCanvas (640,640);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+function draw(){
     background('#397367');
 
-    //Drawing the moon 
-    push();
-    fill('#FFF3D1');
-    ellipse(320,280,150);
-    pop();
+    //Draw land
+    //Back
+    fill('#5E5E5E');
+    rect(0,350,800,300,20)
 
+    //Middle
+    fill('#393D3F ');
+    rect(0,400,800,300,20)
+
+    //Front
+    fill('#333333');
+    rect(0,500,800,300,20)
+
+
+    //Draw moon 
+    push();
+
+    fill('#FFF3D1');
+    ellipse(320,250,200);
+
+    pop();
     //Drawing the mountains
     //Reminders that will be useful:
     //X-axis: 
-            // 1.Increasing x moves the point to the right.
-            //2.Decreasing x moves it to the left.
+    // 1.Increasing x moves the point to the right.
+    //2.Decreasing x moves it to the left.
     //Y-axis:
-        //1. Increasing y moves the point down.
-        //2. Decreasing y moves it up.
+    //1. Increasing y moves the point down.
+    //2. Decreasing y moves it up.
 
-    fill('#000000');
+    //Mountains
+    push();
+
+    fill(0)
+    noStroke();
     triangle(80, 400, 400, 400, 250, 300);
     triangle(200, 400, 600, 400, 400, 270);
     triangle(50, 400, 300, 400, 200, 290);
@@ -49,10 +51,5 @@ function draw() {
     rect (300, 420, 100, 50);
     rect (500, 420, 100, 50);
 
-
-    //Draw land
-    fill('#020122');
-    rect(0,500,800,200)
+    pop();
 }
-
-
