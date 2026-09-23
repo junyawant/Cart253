c3a3caedf7e1c99dc57e0a5a99fe92a7019bd3ec
junya <jomarkson01@gmail.com>
2026-09-22 23:10:50 -0400

    Drawing out the Plus Sign "Flowers"

--- codingassignments/prototype-instructions/prototype-3/abstract-art/js/script.js
+++ codingassignments/prototype-instructions/prototype-3/abstract-art/js/script.js
@@ -21,29 +21,145 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background('#CACF85');
+    background('#dfe2b2');
 
     //Random Plus Signs 
 
-    //Plus Sign Far Left
+    //PS = Plus Sign
+
+    //PS Far Left
+    //Top Square
+    rect (70, 90, 30, 30);
     //Middle Square
-    fill('#EF6F6C');
+    fill('#e47877');
     rect (70, 120, 30, 30);
+     rect (100, 120, 30, 30);
+    //Lower Square
+    rect (70, 150, 30, 30);
     //Left Square
     rect (40, 120, 30, 30);
     //Right Square
-    rect (100, 120, 30, 30);
-    //Lower Square
-    rect (70, 150, 30, 30);
+   
+    //PS Above Far Left One
     //Top Square
-    rect (70, 90, 30, 30);
+    rect (120, 55, 20, 20);
+    //Middle Square
+    rect (120, 75, 20, 20);
+    //Bottom Square
+    rect (120, 95, 20, 20);
+    //Right Square
+    rect (140 ,75, 20, 20);
+    //Left Square
+    rect (100, 75, 20, 20);
+
+    //PS Under Far Left PS'
+    //Top Square
+    rect (120, 300, 30, 30);
+    //Middle Square
+    rect (120, 330, 30, 30);
+    //Bottom Square
+    rect (120, 360, 30, 30);
+    //Right Square
+    rect (150, 330, 30, 30);
+    //Left Square
+    rect (90, 330, 30, 30);
+    
+
+    //PS Top Far Right Next to Big PS
+    //Top Square
+    rect (495, 10, 25, 25,);
+    //Middle Square
+    rect (495, 35, 25, 25);
+    //Bottom Square
+    rect (495, 60, 25, 25);
+    //Right Square
+    rect (520, 35, 25, 25);
+    //Left Square 
+    rect (470, 35, 25, 25);
+
+
+    //Big PS Top Far Right
+    //Right Square
+    rect (590, -10, 38, 38);
+    //Middle Square
+    rect (555, -10, 38, 38);
+    //Left Square
+    rect (520, -10, 38, 38);
+    //Bottom Square
+    rect (555, 30, 38, 38);
+
+    //PS Sign Below Big PS Sign
+    //Top Square
+    rect (570, 240, 29, 29,);
+    //Middle Square
+    rect (570, 270, 29, 29);
+    //Bottom Square
+    rect (570, 300, 29, 29);
+    //Right Square
+    rect (600, 270, 29, 29);
+    //Left Square 
+    rect (540, 270, 29, 29);
+
+    //PS Next to the one Below the Big PS
+    //Top Square
+    rect (425, 225, 25, 25);
+    //Middle Square
+    rect (425, 250, 25, 25);
+    //Bottom Square
+    rect (425, 275, 25, 25);
+    //Right Square
+    rect (450, 250, 25, 25);
+    //Left Square
+    rect (400, 250, 25, 25);
+
+    //PS Below Above (^) PS
+    //Top Square
+    rect (340, 300, 35, 35);
+    //Middle Square
+    rect (340, 270, 35, 35);
+    //Bottom Square
+    rect (340, 335, 35, 35);
+    //Right Square
+    rect (375, 303, 35, 35);
+    //Left Square
+    rect (305, 303, 35, 35);
+
+    //PS Below to the Left of PS Above (^)
+    //Top Square
+    rect (320, 450, 39, 39);
+    //Middle Square
+    rect (320, 490, 39, 39);
+    //Bottom Square
+    rect (320, 530, 39, 39);
+    //Right Square
+    rect (360, 490, 39, 39);
+    //Left Square
+    rect (280, 490, 39, 39);
+
+    //Big PS Next to PS Above (^)
+    //Top Square
+    rect (480, 390, 45, 45);
+    //Middle Square
+    rect (480, 435, 45, 45);
+    //Bottom Square 
+    rect (480, 480, 45, 45);
+    //Right Square 
+    rect (525, 435, 45, 45);
+    //Left Square
+    rect (435, 435, 45, 45);
+
+    //Lines that serves as "Branches"
+    line (100, 10, 10, 10)
 
 
 
-    rect (200, 200, 50, 50);
-    rect (300, 300, 50, 50);
-    rect (400, 400, 50, 50);
-    rect (500, 500, 50, 50);
-    rect (600, 600, 50, 50);
+
+
+
+
+    
+
+
+
 
 }
